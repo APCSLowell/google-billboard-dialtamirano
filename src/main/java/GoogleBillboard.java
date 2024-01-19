1f21bf53a9ab122a189ebf598a75e6f319c10a27
dialtamirano <143028005+dialtamirano@users.noreply.github.com>
2024-01-18 22:54:14 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -13,7 +13,7 @@ for(int i = 2; i < e.length()-10; i++){
 }
 //no draw
   //Finish this function
-public boolean isPrime(double dNum){
+public static boolean isPrime(double dNum){
   if(dNum < 2){
        return false;
   }
